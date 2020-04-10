c6cd7d8aefc02ab4e0cf8e4aaaa9645f9d6e45c0
Rahul Lohra <tgunix@gmail.com>
2020-04-10 17:55:24 +0530

    add profile and add home page route

--- src/Application.kt
+++ src/Application.kt
@@ -1,9 +1,13 @@
 package com.rahul
 
 import com.rahul.datasource.Server
+import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.ContentNegotiation
+import io.ktor.http.HttpStatusCode
 import io.ktor.locations.Locations
+import io.ktor.response.respond
+import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.serialization.json
 import io.ktor.server.engine.embeddedServer
@@ -11,14 +15,17 @@ import io.ktor.server.netty.Netty
 
 fun main(args: Array<String>) {
     val server = Server()
-
-    val nettyServer = embeddedServer(Netty, port = 8083) {
+    val port = System.getenv("PORT")?.toInt() ?: 8083
+    val nettyServer = embeddedServer(Netty, port = port) {
         install(ContentNegotiation) {
             json()
         }
         install(Locations)
         routing {
             server.injectComponents(this)
+            get("/") {
+                call.respond(HttpStatusCode.OK, "Hello")
+            }
         }
     }
     nettyServer.start(wait = true)
